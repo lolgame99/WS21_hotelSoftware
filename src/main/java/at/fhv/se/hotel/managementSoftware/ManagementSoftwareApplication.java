35015f115ed893c744a24ad65137bcecf9ef79df
Unknown <lolgame99@gmail.com>
2021-10-27 08:23:20 +0200

    INIT spring project

---
+++ src/main/java/at/fhv/se/hotel/managementSoftware/ManagementSoftwareApplication.java
@@ -0,0 +1,13 @@
+package at.fhv.se.hotel.managementSoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/fhv/se/hotel/managementSoftware/ManagementSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhv.se.hotel.managementSoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
