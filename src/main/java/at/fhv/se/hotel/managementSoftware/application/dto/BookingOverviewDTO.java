18453cd28638c23265cc461ca4459628bb20ac83
lolgame99 <lolgame99@gmail.com>
2021-10-30 09:41:11 +0200

    [refactor] expanded bookingDTO

---
+++ src/main/java/at/fhv/se/hotel/managementSoftware/application/dto/BookingOverviewDTO.java
@@ -0,0 +1,83 @@
+package at.fhv.se.hotel.managementSoftware.application.dto;
+
+import java.util.Date;
+import java.util.Objects;
+
+
+public class BookingOverviewDTO {
+	private String bookingId;
+	private Date checkInDate;
+	private int roomCount;
+	private int guestCount;
+	private String customerId;
+	
+	
+	public static Builder builder() {
+		return new Builder();
+	}
+			
+	public String getBookingId() {
+		return bookingId;
+	}
+
+	public Date getCheckInDate() {
+		return checkInDate;
+	}
+
+	public int getRoomCount() {
+		return roomCount;
+	}
+
+	public int getGuestCount() {
+		return guestCount;
+	}
+
+	public String getCustomerId() {
+		return customerId;
+	}
+
+	private BookingOverviewDTO() {
+	}
+	
+	public static class Builder{
+		private BookingOverviewDTO instance;
+
+        private Builder() {
+            this.instance = new BookingOverviewDTO();
+        }
+
+        public Builder withId(String id) {
+            this.instance.bookingId = id;
+            return this;
+        }
+
+        public Builder withCheckInDate(Date date) {
+            this.instance.checkInDate = date;
+            return this;
+        } 
+        
+        public Builder withGuestCount(int count) {
+        	this.instance.guestCount  = count;
+        	return this;
+        }
+        
+        public Builder withRoomCount(int count) {
+        	this.instance.roomCount  = count;
+        	return this;
+        }
+        
+        public Builder withCustomerId(String id) {
+        	this.instance.customerId = id;
+        	return this;
+        }
+
+        public BookingOverviewDTO build() {
+            Objects.requireNonNull(this.instance.checkInDate, "checkInDate must be set in BookingOverviewDTO");
+            Objects.requireNonNull(this.instance.bookingId, "bookingId must be set in BookingOverviewDTO");
+            Objects.requireNonNull(this.instance.customerId, "customerId must be set in BookingOverviewDTO");
+            Objects.requireNonNull(this.instance.guestCount, "guestCount must be set in BookingOverviewDTO");
+            Objects.requireNonNull(this.instance.roomCount, "roomCount must be set in BookingOverviewDTO");
+            return this.instance;
+        }
+	}
+}
