1c4b162935b431d14bf65dad5cf54edafa64db23
lolgame99 <lolgame99@gmail.com>
2021-11-02 19:29:03 +0100

    [feat] added customer selection modal

--- src/main/resources/templates/js/createBooking.js
+++ src/main/resources/templates/js/createBooking.js
@@ -5,4 +5,15 @@ $(document).ready(function(){
         $(this).before(newCategoryEntry);
     });
 
+    jQuery("#customerSearch").keyup(function () {
+    var filter = jQuery(this).val();
+    jQuery(".customerList li").each(function () {
+        if (jQuery(this).text().search(new RegExp(filter, "i")) < 0) {
+            jQuery(this).hide();
+        } else {
+            jQuery(this).show()
+        }
+    });
+});
+
 });
