9b8adf03b267452b7fe04c65ed8929befaf10636
lolgame99 <lolgame99@gmail.com>
2021-11-03 17:18:17 +0100

    [feat] added createBooking and dashboard view to controller

--- src/main/java/at/fhv/se/hotel/managementSoftware/view/BookingViewController.java
+++ src/main/java/at/fhv/se/hotel/managementSoftware/view/BookingViewController.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -13,38 +14,57 @@ import org.springframework.web.servlet.ModelAndView;
 
 import at.fhv.se.hotel.managementSoftware.application.api.BookingService;
 import at.fhv.se.hotel.managementSoftware.application.api.CustomerService;
+import at.fhv.se.hotel.managementSoftware.application.api.RoomCategoryService;
 import at.fhv.se.hotel.managementSoftware.application.dto.BookingOverviewDTO;
+import at.fhv.se.hotel.managementSoftware.application.dto.CustomerOverviewDTO;
+import at.fhv.se.hotel.managementSoftware.application.dto.RoomCategoryDTO;
 import at.fhv.se.hotel.managementSoftware.domain.model.Booking;
 import at.fhv.se.hotel.managementSoftware.domain.model.Customer;
 
+@Controller
 public class BookingViewController {
 	
 	
-	private static final String POST_BOOKING_URL = "/booking";
-	private static final String DAILY_CHECKINS_URL ="/booking";
+	private static final String CREATE_BOOKING_URL = "/booking";
+	private static final String DASHBOARD_URL ="/";
 	private static final String ERROR_URL = "/error";
 	
 	//TODO: change name of daily checkin view
-	private static final String DAILY_CHECKINS_VIEW ="dailyBookings";
+	private static final String DASHBOARD_VIEW ="dashboard";
+	private static final String CREATE_BOOKING_VIEW ="createBooking";
 	
 
 	@Autowired
 	private BookingService bookingService;
 	@Autowired
 	private CustomerService customerService;
+	@Autowired
+	private RoomCategoryService roomCategoryService;
 	
-	@GetMapping(DAILY_CHECKINS_URL)
-    public ModelAndView customer(Model model) {
+	@GetMapping(DASHBOARD_URL)
+    public String customer(Model model) {
         
         // TODO: redirect to the error page in case of an error situation - use redirectError("SOME MESSAGE"); for that
         List<BookingOverviewDTO> bookingOverviews = bookingService.getBookingsByDate(LocalDate.now());
         model.addAttribute("bookings", bookingOverviews);
-        return new ModelAndView(DAILY_CHECKINS_VIEW);
+        return DASHBOARD_VIEW;
     }
 	
-	@PostMapping(POST_BOOKING_URL)
+	@GetMapping(CREATE_BOOKING_URL)
+	public String createBooking(Model model) {
+		List<RoomCategoryDTO> roomCategories = roomCategoryService.getAllRoomCategoriesDTO();
+		model.addAttribute("roomCategories", roomCategories);
+		
+		List<CustomerOverviewDTO> customers = customerService.getAllCustomersOverview();
+		model.addAttribute("customers", customers);
+		return CREATE_BOOKING_VIEW;
+	}
+	
+	/*
+	@PostMapping(CREATE_BOOKING_VIEW)
 	  public ModelAndView greetingSubmit(@ModelAttribute Booking booking, @ModelAttribute Customer customer,Model model) {
 		bookingService.addBooking(booking);
 	    return new ModelAndView("redirect:" + DAILY_CHECKINS_URL);
 	  }
+	  */
 }
