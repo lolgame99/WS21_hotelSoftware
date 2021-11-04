a9a5c540d7344e757ebd811afb483d44e1182181
lolgame99 <lolgame99@gmail.com>
2021-11-04 18:32:14 +0100

    [feat] added uuid generation into testdata

--- src/main/java/at/fhv/se/hotel/managementSoftware/TestData.java
+++ src/main/java/at/fhv/se/hotel/managementSoftware/TestData.java
@@ -1,6 +1,7 @@
 package at.fhv.se.hotel.managementSoftware;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -31,22 +32,26 @@ public class TestData implements ApplicationRunner{
 	
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		customerRepository.addCustomer(new Customer("1", "Ulrich", "Vogler", LocalDate.of(1988, 7, 21), new Address("Kantstra�e", "32", "Rochlitz", "09301", "Deutschland"), "UlrichVogler@rhyta.com", "+493737105579", Gender.MALE));
-		customerRepository.addCustomer(new Customer("2", "Michelle", "Eichelberger", LocalDate.of(1991, 2, 15), new Address("Luebecker Strasse", "62", "Seubersdorf", "92358 ", "Deutschland"), "MichelleEichelberger@rhyta.com", "++499497826628", Gender.FEMALE));
+		String[] customerUUID = {UUID.randomUUID().toString().toUpperCase(),UUID.randomUUID().toString().toUpperCase()};
+		String[] categoryUUID = {UUID.randomUUID().toString().toUpperCase(),UUID.randomUUID().toString().toUpperCase(),UUID.randomUUID().toString().toUpperCase()};
+		String[] bookingUUID = {UUID.randomUUID().toString().toUpperCase(),UUID.randomUUID().toString().toUpperCase(),UUID.randomUUID().toString().toUpperCase()};
 		
-		roomCategoryRepository.addRoomCategory(new RoomCategory("1", "Single Room", 1));
-		roomCategoryRepository.addRoomCategory(new RoomCategory("2", "Double Room", 2));
-		roomCategoryRepository.addRoomCategory(new RoomCategory("3", "Family Suite", 4));
+		customerRepository.addCustomer(new Customer(customerUUID[0], "Ulrich", "Vogler", LocalDate.of(1988, 7, 21), new Address("Kantstrasse", "32", "Rochlitz", "09301", "Deutschland"), "UlrichVogler@rhyta.com", "+493737105579", Gender.MALE));
+		customerRepository.addCustomer(new Customer(customerUUID[1], "Michelle", "Eichelberger", LocalDate.of(1991, 2, 15), new Address("Luebecker Strasse", "62", "Seubersdorf", "92358 ", "Deutschland"), "MichelleEichelberger@rhyta.com", "++499497826628", Gender.FEMALE));
+		
+		roomCategoryRepository.addRoomCategory(new RoomCategory(categoryUUID[0], "Single Room", 1));
+		roomCategoryRepository.addRoomCategory(new RoomCategory(categoryUUID[1], "Double Room", 2));
+		roomCategoryRepository.addRoomCategory(new RoomCategory(categoryUUID[2], "Family Suite", 4));
 		
 		
 	
-		bookingRepository.addBooking(new Booking("1", LocalDate.of(2021, 11, 4), LocalDate.of(2021, 11, 10), "5555555555554444", customerRepository.getCustomerById("1").get(), 2, BookingStatus.CONFIRMED)
-				.addCategory(roomCategoryRepository.getRoomCategoryById("2").get(), 1));
-		bookingRepository.addBooking(new Booking("2", LocalDate.of(2021, 11, 3), LocalDate.of(2021, 11, 15), "5555555555554444", customerRepository.getCustomerById("2").get(), 6, BookingStatus.PENDING)
-				.addCategory(roomCategoryRepository.getRoomCategoryById("3").get(), 1)
-				.addCategory(roomCategoryRepository.getRoomCategoryById("2").get(), 1));
-		bookingRepository.addBooking(new Booking("3", LocalDate.of(2021, 11, 10), LocalDate.of(2021, 11, 20), "5555555555554444", customerRepository.getCustomerById("1").get(), 2, BookingStatus.PAID)
-				.addCategory(roomCategoryRepository.getRoomCategoryById("1").get(), 2));
+		bookingRepository.addBooking(new Booking(bookingUUID[0], LocalDate.of(2021, 11, 4), LocalDate.of(2021, 11, 10), "5555555555554444", customerRepository.getCustomerById(customerUUID[0]).get(), 2, BookingStatus.CONFIRMED)
+				.addCategory(roomCategoryRepository.getRoomCategoryById(categoryUUID[1]).get(), 1));
+		bookingRepository.addBooking(new Booking(bookingUUID[1], LocalDate.of(2021, 11, 3), LocalDate.of(2021, 11, 15), "5555555555554444", customerRepository.getCustomerById(customerUUID[1]).get(), 6, BookingStatus.PENDING)
+				.addCategory(roomCategoryRepository.getRoomCategoryById(categoryUUID[2]).get(), 1)
+				.addCategory(roomCategoryRepository.getRoomCategoryById(categoryUUID[1]).get(), 1));
+		bookingRepository.addBooking(new Booking(bookingUUID[2], LocalDate.of(2021, 11, 10), LocalDate.of(2021, 11, 20), "5555555555554444", customerRepository.getCustomerById(customerUUID[0]).get(), 2, BookingStatus.PAID)
+				.addCategory(roomCategoryRepository.getRoomCategoryById(categoryUUID[0]).get(), 2));
 		
 		
 		
