e40ab3612e602161e658e1426ab3ff1cf9572583
lolgame99 <lolgame99@gmail.com>
2021-11-04 18:16:37 +0100

    [feat] added CustomerDetails DTO and service

--- src/main/java/at/fhv/se/hotel/managementSoftware/application/api/CustomerService.java
+++ src/main/java/at/fhv/se/hotel/managementSoftware/application/api/CustomerService.java
@@ -3,9 +3,11 @@ package at.fhv.se.hotel.managementSoftware.application.api;
 import java.util.List;
 import java.util.Optional;
 
+import at.fhv.se.hotel.managementSoftware.application.dto.CustomerDetailsDTO;
 import at.fhv.se.hotel.managementSoftware.application.dto.CustomerOverviewDTO;
 
 public interface CustomerService {
 	public List<CustomerOverviewDTO> getAllCustomersOverview();
 	public Optional<CustomerOverviewDTO> getCustomerOverviewById(String id);
+	public Optional<CustomerDetailsDTO> getCustomerDetailsById(String id);
 }
--- src/main/java/at/fhv/se/hotel/managementSoftware/application/impl/CustomerServiceImpl.java
+++ src/main/java/at/fhv/se/hotel/managementSoftware/application/impl/CustomerServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import at.fhv.se.hotel.managementSoftware.application.api.CustomerService;
+import at.fhv.se.hotel.managementSoftware.application.dto.CustomerDetailsDTO;
 import at.fhv.se.hotel.managementSoftware.application.dto.CustomerOverviewDTO;
 import at.fhv.se.hotel.managementSoftware.domain.model.Customer;
 import at.fhv.se.hotel.managementSoftware.domain.repositories.CustomerRepository;
@@ -49,6 +50,29 @@ public class CustomerServiceImpl implements CustomerService{
 					.withMiddleName(customer.get().getMiddleName())
 					.withId(customer.get().getCustomerId())
 					.withBirthdate(customer.get().getBirthdate())
+					.build());
+		}
+		
+		return customerDTO;
+	}
+
+	@Override
+	public Optional<CustomerDetailsDTO> getCustomerDetailsById(String id) {
+		Optional<Customer> customer = customerRepository.getCustomerById(id);
+		Optional<CustomerDetailsDTO> customerDTO = Optional.empty();
+		
+		if (customer.isPresent()) {
+			customerDTO = Optional.of(CustomerDetailsDTO.builder()
+					.withFirstName(customer.get().getFirstName())
+					.withLastName(customer.get().getLastName())
+					.withMiddleName(customer.get().getMiddleName())
+					.withId(customer.get().getCustomerId())
+					.withBirthdate(customer.get().getBirthdate())
+					.withAddress(customer.get().getAddress())
+					.withEmail(customer.get().getEmail())
+					.withPhoneNumber(customer.get().getPhoneNumber())
+					.withGender(customer.get().getGender())
+					.withBillingAddress(customer.get().getBillingAddress())
 					.build());
 		}
 		
