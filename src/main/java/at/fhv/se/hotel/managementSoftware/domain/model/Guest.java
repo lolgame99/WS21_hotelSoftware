cc1542e1990865cfcae590d49efe00c77f646501
Barbi <barbaros.teker@gmx.net>
2021-11-10 09:30:51 +0100

    [feat] created Stay DO
    Stay Domain Object
    
    Co-Authored-By: eha7244 <93255324+eha7244@users.noreply.github.com>

---
+++ src/main/java/at/fhv/se/hotel/managementSoftware/domain/model/Guest.java
@@ -0,0 +1,7 @@
+package at.fhv.se.hotel.managementSoftware.domain.model;
+
+public class Guest {
+	
+	
+
+}
---
+++ src/main/java/at/fhv/se/hotel/managementSoftware/domain/model/Stay.java
@@ -0,0 +1,71 @@
+package at.fhv.se.hotel.managementSoftware.domain.model;
+
+import java.time.LocalDate;
+
+public class Stay {
+	
+	private String stayId;
+	private LocalDate checkInDate;
+	private LocalDate checkOutDate;
+	private int numberOfAdults;
+	private int numberOfChildren;
+	private String creditCardNumber; 
+	private String bookingId;
+	private String customerId;
+
+	public Stay(String stayId, LocalDate checkInDate, LocalDate checkOutDate,
+			String creditCardNumber, String bookingId, String customerId)
+	{
+		this.stayId = stayId;
+		this.checkInDate = checkInDate;
+		this.checkOutDate = checkOutDate; 
+		this.creditCardNumber = creditCardNumber;
+		this.bookingId = bookingId; 
+		this.customerId = customerId;
+	}
+
+	public void addNumberOfAdults(int numberOfAdults) {
+		this.numberOfAdults = numberOfAdults;
+	}
+	
+	public void addNumberOfChildren(int numberOfChildren) {
+		this.numberOfChildren = numberOfChildren;
+	}
+	
+	public String getStayId() {
+		return stayId;
+	}
+
+	public LocalDate getCheckInDate() {
+		return checkInDate;
+	}
+
+	public LocalDate getCheckOutDate() {
+		return checkOutDate;
+	}
+
+	public String getCreditCardNumber() {
+		return creditCardNumber;
+	}
+
+	public String getBookingId() {
+		return bookingId;
+	}
+
+	public String getCustomerId() {
+		return customerId;
+	}
+
+	public int getNumberOfAdults() {
+		return numberOfAdults;
+	}
+
+	public int getNumberOfChildren() {
+		return numberOfChildren;
+	}
+	
+	
+	
+	
+}
+
