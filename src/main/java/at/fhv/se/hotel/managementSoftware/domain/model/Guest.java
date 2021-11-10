e57d6d40273f5ad55d167d98a885a9ea42e02a36
Barbi <barbaros.teker@gmx.net>
2021-11-10 09:38:30 +0100

    [feat] created Guest DO
    Guest Domain object
    
    Co-Authored-By: eha7244 <93255324+eha7244@users.noreply.github.com>

--- src/main/java/at/fhv/se/hotel/managementSoftware/domain/model/Guest.java
+++ src/main/java/at/fhv/se/hotel/managementSoftware/domain/model/Guest.java
@@ -2,6 +2,34 @@ package at.fhv.se.hotel.managementSoftware.domain.model;
 
 public class Guest {
 	
+	private String guestId;
+	private String name;
+	private String phoneNumber;
+	private String stayId;
 	
+	public Guest(String guestId, String name, String phoneNumber, String stayId)
+	{
+		this.guestId = guestId;
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.stayId = stayId;
+	}
 
+	public String getGuestId() {
+		return guestId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public String getStayId() {
+		return stayId;
+	}
+
+	
 }
