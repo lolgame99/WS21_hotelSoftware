17a95e8d176532648a845fbd20ed45c70c605d01
Zikrihan <93253402+Zikrihan@users.noreply.github.com>
2021-11-10 20:47:49 +0100

    [WIP]stayOverview and createStay html-design and js [feat]dashboard and createbooking navbar dropdown update

---
+++ src/main/resources/static/js/createStay.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	var categoryCounter = 0;
+	
+    $(".addCategoryBtn").click(function(){
+        var newCategoryEntry = $('.categoryEntryEmpty').clone();
+        newCategoryEntry.removeClass("categoryEntryEmpty d-none");
+		newCategoryEntry.addClass("categoryEntry");
+		newCategoryEntry.find(".categoryAmounts")[0].setAttribute("name","categoryAmounts["+ categoryCounter+"]");
+		newCategoryEntry.find(".categoryValues")[0].setAttribute("name","categoryValues["+ categoryCounter+"]");
+        $(this).before(newCategoryEntry);
+		categoryCounter++;
+    });
+
+	$(".removeCategoryBtn").click(function(){
+		categoryCounter--;
+        $(".categoryEntry").last().remove();
+    });
+
+	$(".resetBtn").click(function(){
+        var baseUrl = window.location.href.split("?")[0];
+		$(location).attr('href',baseUrl);
+    });
+
+    jQuery("#customerSearch").keyup(function () {
+        var filter = jQuery(this).val();
+        jQuery(".customerList li").each(function () {
+            if (jQuery(this).text().search(new RegExp(filter, "i")) < 0) {
+                jQuery(this).hide();
+            } else {
+                jQuery(this).show();
+            }
+        });
+    });
+
+});
