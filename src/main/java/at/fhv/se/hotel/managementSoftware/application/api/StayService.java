cf58557ca5f75060ef9fe0c886e56d952abb3bed
lolgame99 <lolgame99@gmail.com>
2021-11-12 10:06:09 +0100

    [feat] added wip StayViewController

--- src/main/java/at/fhv/se/hotel/managementSoftware/application/api/StayService.java
+++ src/main/java/at/fhv/se/hotel/managementSoftware/application/api/StayService.java
@@ -4,8 +4,10 @@ import java.time.LocalDate;
 import java.util.List;
 
 import at.fhv.se.hotel.managementSoftware.application.dto.StayDetailsDTO;
+import at.fhv.se.hotel.managementSoftware.view.forms.StayData;
 
 public interface StayService {
 	public List<StayDetailsDTO> getAllStays();
 	public List<StayDetailsDTO> getCurrentStays(LocalDate date);
+	public void addStayFromData(StayData stayData, LocalDate convertedCheckInDate, LocalDate convertedCheckOutDate, LocalDate convertedBirthDate);
 }
