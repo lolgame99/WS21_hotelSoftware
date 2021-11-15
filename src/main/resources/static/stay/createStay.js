4222014d24a14359c7ea1c0bb6819904ec3828ab
lolgame99 <lolgame99@gmail.com>
2021-11-15 10:16:12 +0100

    [fix] moved createStay

--- src/main/resources/static/stay/createStay.js
+++ src/main/resources/static/stay/createStay.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
 	var categoryCounter = 0;
 	
+	var now = new Date();
+	var day = ("0" + now.getDate()).slice(-2);
+	var month = ("0" + (now.getMonth() + 1)).slice(-2);
+	var today = now.getFullYear()+"-"+(month)+"-"+(day);
+	
+	$('input[name=checkInDate]').val(today);
+	$('input[name=checkInDate]').prop('disabled',true);
+	
     $(".addCategoryBtn").click(function(){
         var newCategoryEntry = $('.categoryEntryEmpty').clone();
         newCategoryEntry.removeClass("categoryEntryEmpty d-none");
@@ -15,6 +23,21 @@ $(document).ready(function(){
 		categoryCounter--;
         $(".categoryEntry").last().remove();
     });
+
+	$("input[type=radio][name=guest]").change(function() {
+	    if (this.value == "newGuest") {
+	        $(".guest").each(function () {
+            	$(this).removeClass("d-none");
+				$(this).find("input").prop('required',true);
+        	});
+	    }
+	    else if (this.value == "sameAsCustomer") {
+			 $(".guest").each(function () {
+            	$(this).addClass("d-none");
+				$(this).find("input").prop('required',false);
+        	});
+	    }
+	});
 
 	$(".resetBtn").click(function(){
         var baseUrl = window.location.href.split("?")[0];
