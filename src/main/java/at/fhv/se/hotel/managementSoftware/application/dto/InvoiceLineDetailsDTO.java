32f1132c6912204eb67453ab0e173056e1ac284c
dku7033 <dku7033@students.fhv.at>
2021-11-25 13:08:01 +0100

    [feat]created InvoiceLineDetailsDTO
    Co-Authored-By: Batu38 <93723536+Batu38@users.noreply.github.com>

---
+++ src/main/java/at/fhv/se/hotel/managementSoftware/application/dto/InvoiceLineDetailsDTO.java
@@ -0,0 +1,46 @@
+package at.fhv.se.hotel.managementSoftware.application.dto;
+
+import at.fhv.se.hotel.managementSoftware.domain.model.InvoiceId;
+import at.fhv.se.hotel.managementSoftware.domain.model.InvoiceLine;
+
+public class InvoiceLineDetailsDTO {
+	private InvoiceId invoiceId;
+	private int count;
+	private String description;
+	private String name;
+	private String line;
+	
+	private InvoiceLineDetailsDTO() {
+	}
+	
+	public static InvoiceLineDetailsDTO createsFromInvoiceLine (InvoiceLine invoiceLine) {
+		InvoiceLineDetailsDTO dto = new InvoiceLineDetailsDTO();
+		dto.invoiceId = invoiceLine.getInvoiceId();
+		dto.count = invoiceLine.getCount();
+		dto.description = invoiceLine.getDescription();
+		dto.name = invoiceLine.getName();
+		dto.line = invoiceLine.getLine();
+		
+		return dto;
+	}
+
+	public InvoiceId getInvoiceId() {
+		return invoiceId;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getLine() {
+		return line;
+	}
+}
