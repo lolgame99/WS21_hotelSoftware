4be14e01f1c87a5da2d693f8bb79e8b5a8163b04
yce5586 <yce5586@students.fhv.at>
2021-12-14 19:41:24 +0100

    [feat] finalInvoice safe as pdf -> pdfname = invoiceid

--- src/main/resources/static/js/createFinalInvoice.js
+++ src/main/resources/static/js/createFinalInvoice.js
@@ -1,24 +1,21 @@
 function pdf(){
-        var element = document.getElementById('invoice');
-var opt = {
-  margin:       0,
-  filename:     'myfile.pdf',
-  html2canvas:  { scale: 3 },
-  jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' }
-};
+        	var element = document.getElementById('invoice');
+  	var opt = {
+  			margin:       0,
+  			filename:     pdfName,
+  			html2canvas:  { scale: 10 },
+  			jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' }
+		};
 
-// New Promise-based usage:
-html2pdf().set(opt).from(element).save();
-
-
-    }
+		// New Promise-based usage:
+		html2pdf().set(opt).from(element).save();
+  	}
     
-    function GoBackWithRefresh(event) {
-    	   if ('referrer' in document) {
-    	      window.location = document.referrer;
-    	      /* OR */
-    	      //location.replace(document.referrer);
-    	   } else {
-    	      window.history.back();
-    	   }
-    	}
+function GoBackWithRefresh(event) {
+	if ('referrer' in document) {
+    	window.location = document.referrer;
+   	}
+	else {
+    	window.history.back();
+   	}
+}
