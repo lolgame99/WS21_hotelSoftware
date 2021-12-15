5c1ae87326fed08ee9281eb22bf591f4ed875ea3
lolgame99 <lolgame99@gmail.com>
2021-12-15 17:25:25 +0100

    [feat] added roomOverview incl. filter function

--- src/main/java/at/fhv/se/hotel/managementSoftware/domain/enums/RoomStatus.java
+++ src/main/java/at/fhv/se/hotel/managementSoftware/domain/enums/RoomStatus.java
@@ -1,5 +1,18 @@
 package at.fhv.se.hotel.managementSoftware.domain.enums;
 
 public enum RoomStatus {
-	AVAILABLE, OCCUPIED, CLEANING, MAINTENANCE;
+	AVAILABLE("Available"),
+	OCCUPIED("Occupied"),
+	CLEANING("Cleaning"),
+	MAINTENANCE("Maintenance");
+	
+	private final String displayValue;
+    
+    private RoomStatus(String displayValue) {
+        this.displayValue = displayValue;
+    }
+    
+    public String getDisplayValue() {
+        return displayValue;
+    }
 }
--- src/main/resources/static/js/roomOverview.js
+++ src/main/resources/static/js/roomOverview.js
@@ -12,26 +12,38 @@ $(document).ready(function(){
             }
         });
     });
-	$('.dropdown-menu a').on('click', function(event) {
-	   var options = [];
-
-	   var $target = $(event.currentTarget),
-	       val = $target.attr('data-value'),
-	       $inp = $target.find('input'),
-	       idx;
 	
-	   if ((idx = options.indexOf(val)) > -1) {
-	      options.splice(idx, 1);
-	      setTimeout(function() { $inp.prop('checked', false)}, 0);
-	   } else {
-	      options.push(val);
-	      setTimeout(function() {$inp.prop('checked', true)}, 0);
-	   }
+	var options = [];
+	$( '.dropdown-menu a' ).on( 'click', function( event ) {
+	    var  $target = $( event.currentTarget ),
+	        val = $target.attr( 'data-value' ),
+	        $inp = $target.find( 'input' ),
+	        idx;
 	
-	   $(event.target).blur();
+	    if ( ( idx = options.indexOf( val ) ) > -1 ) {
+	       options.splice( idx, 1 );
+	       setTimeout( function() { $inp.prop( 'checked', false ) }, 0);
+	    } else {
+	       options.push( val );
+	       setTimeout( function() { $inp.prop( 'checked', true ) }, 0);
+	    }
+	
+	    $( event.target ).blur();
 	      
-	   console.log(options);
-	   return false;
+		console.log( options );
+		if(options.length != 0){
+			$(".roomStatus").each(function() {
+				if($.inArray($(this).html(),options) == -1){
+					$(this).parent().hide();
+				}else{
+					$(this).parent().show();
+				}
+			});
+		}else{
+			$(".roomStatus").each(function() {
+					$(this).parent().show();
+			});
+		}
+		
 	});
-	
 });
