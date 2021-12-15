be24ee745457f784652ec3554423cd11c10929ee
lolgame99 <lolgame99@gmail.com>
2021-12-15 13:57:55 +0100

    [refactor] moved invoicenumber variable into javascript file

--- src/main/resources/static/js/createFinalInvoice.js
+++ src/main/resources/static/js/createFinalInvoice.js
@@ -1,15 +1,18 @@
+
 function pdf(){
-        	var element = document.getElementById('invoice');
-  	var opt = {
-  			margin:       0,
-  			filename:     pdfName,
-  			html2canvas:  { scale: 10 },
-  			jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' }
+	var pdfName = $('.invoiceNumber').html().replace(' ','');
+	console.log(pdfName);
+	var element = document.getElementById('invoice');
+	var opt = {
+			margin:       0,
+			filename:     pdfName,
+			html2canvas:  { scale: 10 },
+			jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' }
 		};
 
-		// New Promise-based usage:
-		html2pdf().set(opt).from(element).save();
-  	}
+	// New Promise-based usage:
+	html2pdf().set(opt).from(element).save();
+}
     
 function GoBackWithRefresh(event) {
 	if ('referrer' in document) {
