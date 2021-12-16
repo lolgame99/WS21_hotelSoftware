4cbee7b83f60ca5ac833ad3875a81ec0e4c4309c
lolgame99 <lolgame99@gmail.com>
2021-12-16 10:32:07 +0100

    [feat] added test for invoiceCreation via data

--- src/test/java/at/fhv/se/hotel/managementSoftware/integration/application/InvoiceServiceTest.java
+++ src/test/java/at/fhv/se/hotel/managementSoftware/integration/application/InvoiceServiceTest.java
@@ -1,6 +1,7 @@
 package at.fhv.se.hotel.managementSoftware.integration.application;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
@@ -8,6 +9,7 @@ import static org.mockito.ArgumentMatchers.any;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,20 +23,48 @@ import org.springframework.test.context.ActiveProfiles;
 import at.fhv.se.hotel.managementSoftware.application.api.CustomerService;
 import at.fhv.se.hotel.managementSoftware.application.api.InvoiceLineService;
 import at.fhv.se.hotel.managementSoftware.application.api.InvoiceService;
+import at.fhv.se.hotel.managementSoftware.application.dto.BookingDetailsDTO;
+import at.fhv.se.hotel.managementSoftware.application.dto.CustomerDetailsDTO;
+import at.fhv.se.hotel.managementSoftware.application.dto.GuestDetailsDTO;
 import at.fhv.se.hotel.managementSoftware.application.dto.InvoiceDetailsDTO;
 import at.fhv.se.hotel.managementSoftware.application.dto.InvoiceLineDetailsDTO;
+import at.fhv.se.hotel.managementSoftware.application.dto.PriceDetailsDTO;
+import at.fhv.se.hotel.managementSoftware.application.dto.RoomAssignmentDTO;
+import at.fhv.se.hotel.managementSoftware.application.dto.RoomCategoryDTO;
+import at.fhv.se.hotel.managementSoftware.application.dto.RoomDTO;
+import at.fhv.se.hotel.managementSoftware.application.dto.StayDetailsDTO;
+import at.fhv.se.hotel.managementSoftware.domain.enums.BookingStatus;
 import at.fhv.se.hotel.managementSoftware.domain.enums.Gender;
 import at.fhv.se.hotel.managementSoftware.domain.enums.PaymentType;
+import at.fhv.se.hotel.managementSoftware.domain.enums.RoomStatus;
+import at.fhv.se.hotel.managementSoftware.domain.exceptions.InvalidBookingException;
 import at.fhv.se.hotel.managementSoftware.domain.exceptions.InvalidCustomerException;
+import at.fhv.se.hotel.managementSoftware.domain.exceptions.InvalidInvoiceException;
 import at.fhv.se.hotel.managementSoftware.domain.model.IndividualCustomer;
+import at.fhv.se.hotel.managementSoftware.domain.model.Booking;
+import at.fhv.se.hotel.managementSoftware.domain.model.BookingId;
+import at.fhv.se.hotel.managementSoftware.domain.model.Customer;
 import at.fhv.se.hotel.managementSoftware.domain.model.CustomerId;
+import at.fhv.se.hotel.managementSoftware.domain.model.Guest;
+import at.fhv.se.hotel.managementSoftware.domain.model.GuestId;
 import at.fhv.se.hotel.managementSoftware.domain.model.Invoice;
 import at.fhv.se.hotel.managementSoftware.domain.model.InvoiceId;
 import at.fhv.se.hotel.managementSoftware.domain.model.InvoiceLine;
+import at.fhv.se.hotel.managementSoftware.domain.model.Price;
+import at.fhv.se.hotel.managementSoftware.domain.model.Room;
+import at.fhv.se.hotel.managementSoftware.domain.model.RoomAssignment;
+import at.fhv.se.hotel.managementSoftware.domain.model.RoomAssignmentId;
+import at.fhv.se.hotel.managementSoftware.domain.model.RoomCategory;
+import at.fhv.se.hotel.managementSoftware.domain.model.RoomCategoryId;
+import at.fhv.se.hotel.managementSoftware.domain.model.RoomId;
+import at.fhv.se.hotel.managementSoftware.domain.model.Stay;
 import at.fhv.se.hotel.managementSoftware.domain.model.StayId;
+import at.fhv.se.hotel.managementSoftware.domain.repositories.CustomerRepository;
 import at.fhv.se.hotel.managementSoftware.domain.repositories.InvoiceRepository;
+import at.fhv.se.hotel.managementSoftware.domain.repositories.RoomAssignmentRepository;
 import at.fhv.se.hotel.managementSoftware.domain.valueObjects.Address;
 import at.fhv.se.hotel.managementSoftware.domain.valueObjects.InvoiceCustomer;
+import at.fhv.se.hotel.managementSoftware.view.forms.InvoiceData;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -48,6 +78,12 @@ public class InvoiceServiceTest {
 	
 	@MockBean
 	private InvoiceLineService invoiceLineService;
+	
+	@MockBean
+	private RoomAssignmentRepository roomAssignmentRepository;
+	
+	@MockBean
+	private CustomerRepository customerRepository;
 	
 	@Test
 	void when_getAll_invoices_return_all() throws InvalidCustomerException {
@@ -158,5 +194,69 @@ public class InvoiceServiceTest {
 		assertEquals(invoice.get().getStayId().getId(), dto.get().getStayId().getId());
 		assertEquals(invoice.get().getPaymentType(), dto.get().getPaymentType());
 		assertEquals(invoice.get().getSum(), dto.get().getSum());
+	}
+	
+	@Test	void when_given_invoiceData_create_invoice() throws Exception {
+		//given
+		StayId stayId = new StayId("123");
+		LocalDate checkInDate = LocalDate.now();
+		LocalDate checkOutDate = LocalDate.now().plusDays(7);
+		int guestCount = 3;
+		String creditCardNumber = "0201133211";
+		BookingId bookingId = new BookingId("B12");
+		CustomerId customerId = new CustomerId("122");
+		GuestId guestId = new GuestId("133");
+		
+		//given from RoomCategory
+        RoomCategoryId categoryId = new RoomCategoryId("1");
+        String categoryName = "Family Suite";
+        int bedNumber = 2;
+        HashMap <RoomCategory, Integer> categoryCount = new HashMap<>();
+        categoryCount.put(RoomCategory.createWithoutDescription(categoryId, categoryName, bedNumber), 3);
+		
+        RoomAssignmentId roomAssignmentId = new RoomAssignmentId("1");
+        RoomId roomId = new RoomId("AA");
+        Booking booking = Booking.create(bookingId, checkInDate, checkOutDate, creditCardNumber, "11/22" , customerId, guestCount, BookingStatus.PAID, categoryCount);
+		Stay stay = Stay.createFromBooking(stayId, booking, guestId);
+        
+        Customer customer = IndividualCustomer.create(customerId, "Ulrich", "Vogler", LocalDate.of(1988, 7, 21), new Address("Kantstrasse", "32", "Rochlitz", "09301", "Germany"), "UlrichVogler@rhyta.com", "+493737105579", Gender.MALE);
+        Guest guest = Guest.create(guestId, "Johnny" , "Muster", "43546846546");
+		RoomCategory cat = RoomCategory.createWithDescription(categoryId, categoryName, bedNumber, categoryName);
+        Room room = Room.create(roomId, RoomStatus.AVAILABLE, cat);
+        Price price = Price.create(categoryId, new BigDecimal("250"), checkInDate, checkOutDate);
+        RoomAssignment roomAssignment = RoomAssignment.create(roomAssignmentId, roomId, stay);
+        List<RoomAssignmentDTO> roomAssignmentDTOs = new ArrayList<RoomAssignmentDTO>();
+        roomAssignmentDTOs.add(RoomAssignmentDTO.createFromRoomAssignment(roomAssignment, RoomDTO.createFromRoom(room, RoomCategoryDTO.createFromCategory(cat, PriceDetailsDTO.createFromPrice(price)))));
+        
+		InvoiceData data = new InvoiceData();
+		data.addInfo(StayDetailsDTO.createFromStay(stay, Optional.of(BookingDetailsDTO.createFromBooking(booking, CustomerDetailsDTO.createFromCustomer(customer))), CustomerDetailsDTO.createFromCustomer(customer), GuestDetailsDTO.createFromGuest(guest), roomAssignmentDTOs));
+		
+		ArrayList<Boolean> selectedPositions = new ArrayList<Boolean>();
+		selectedPositions.add(true);
+		data.setToPay(selectedPositions);
+		
+		ArrayList<String> names = new ArrayList<String>();
+		ArrayList<String> descriptions = new ArrayList<String>();
+		ArrayList<String> assignmentIds = new ArrayList<String>();
+		ArrayList<BigDecimal> prices = new ArrayList<BigDecimal>();
+		names.add("Testpositions");
+		descriptions.add("Testposition Description");
+		prices.add(BigDecimal.valueOf(100));
+		assignmentIds.add(roomAssignment.getRoomAssignmentId().getId());
+		
+		data.setDescriptions(descriptions);
+		data.setNames(names);
+		data.setPrices(prices);
+		data.setAssignmentIds(assignmentIds);
+		data.setPaymentType(PaymentType.CASH);
+		
+		Mockito.when(roomAssignmentRepository.getRoomAssignmentsById(any(RoomAssignmentId.class))).thenReturn(Optional.of(roomAssignment));
+		Mockito.when(customerRepository.getCustomerById(any(CustomerId.class))).thenReturn(Optional.of(customer));
+		
+		data.validate();
+		
+		//when.. then
+		assertDoesNotThrow(() -> invoiceService.addInvoiceFromData(data));
+		
 	}
 }
