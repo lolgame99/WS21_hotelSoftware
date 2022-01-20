5e37efcc5e1ddbcdd3dfa6f3e5a1ec13b3110e7c
lolgame99 <lolgame99@gmail.com>
2022-01-20 13:49:28 +0100

    [fix] removed unused imports

--- src/main/java/at/fhv/se/hotel/managementSoftware/application/dto/BookingAssignmentDTO.java
+++ src/main/java/at/fhv/se/hotel/managementSoftware/application/dto/BookingAssignmentDTO.java
@@ -1,15 +1,5 @@
 package at.fhv.se.hotel.managementSoftware.application.dto;
 
-import java.time.LocalDate;
-
-import at.fhv.se.hotel.managementSoftware.domain.enums.PaymentStatus;
-import at.fhv.se.hotel.managementSoftware.domain.model.BookingAssignment;
-import at.fhv.se.hotel.managementSoftware.domain.model.BookingAssignmentId;
-import at.fhv.se.hotel.managementSoftware.domain.model.BookingId;
-import at.fhv.se.hotel.managementSoftware.domain.model.RoomAssignment;
-import at.fhv.se.hotel.managementSoftware.domain.model.RoomAssignmentId;
-import at.fhv.se.hotel.managementSoftware.domain.model.RoomCategory;
-
 public class BookingAssignmentDTO {
 	private String category;
 	private int count;
